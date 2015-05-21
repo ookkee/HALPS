8f0d1dca6c1efbbec037eb6e0f4dc70025e8ad9b
Mikael Laine <mikael.laine@students.turkuamk.fi>
2015-05-21 13:07:33 +0900

    Add scrollview, add reference time to timestamp

--- src/jp/hiralab/halps/SensorDataActivity.java
+++ src/jp/hiralab/halps/SensorDataActivity.java
@@ -12,7 +12,6 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.Environment;
-import android.text.method.KeyListener;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -24,7 +23,9 @@ public class SensorDataActivity extends Activity implements SensorEventListener
     private Sensor mAccelerometer, mGravity, mOrientation;
     TextView vwAccData, vwGraData, vwOriData;
     String strAccFile, strGraFile, strOriFile;
+    String strFileName = new String("");
     boolean recording = false;
+    long referenceTime;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -40,7 +41,7 @@ public class SensorDataActivity extends Activity implements SensorEventListener
         // Query sensor services
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         List<Sensor> sensorList = mSensorManager.getSensorList(Sensor.TYPE_ALL);
-        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         mGravity = mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
         mOrientation = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
 
@@ -58,6 +59,8 @@ public class SensorDataActivity extends Activity implements SensorEventListener
         else {
             vwSensorInfo.setText("No sensors found!");
         }
+        if(isExternalStorageWritable())
+            vwSensorInfo.append("\next storage is writable!\n");
 
         /*
         if (mAccelerometer != null) {
@@ -92,19 +95,22 @@ public class SensorDataActivity extends Activity implements SensorEventListener
         EditText fileNameInput = (EditText) findViewById(R.id.filenameinput);
         if(recording) {
             // Stop recording and write to file
-            String strFileName = new String(fileNameInput.getText().toString());
+            referenceTime = 0;
             writeData(strFileName + "_acc.csv", strAccFile);
-            strAccFile = "";
             writeData(strFileName + "_gra.csv", strGraFile);
-            strGraFile = "";
             writeData(strFileName + "_ori.csv", strOriFile);
+            strAccFile = "";
+            strGraFile = "";
             strOriFile = "";
-            fileNameInput.setKeyListener((KeyListener)fileNameInput.getTag());
+            fileNameInput.setText(strFileName);
             btn.setText("Start");
         }
         else {
             // Start recording
-            fileNameInput.setTag(fileNameInput.getKeyListener());
+            referenceTime = System.nanoTime();
+            strFileName = fileNameInput.getText().toString();
+            fileNameInput.setText("");
+            fileNameInput.setHint("recording to " + strFileName);
             btn.setText("Stop");
         }
         recording = !recording;
@@ -119,56 +125,42 @@ public class SensorDataActivity extends Activity implements SensorEventListener
     public void onSensorChanged(SensorEvent event) {
 
         Sensor sensor = event.sensor;
-        if (sensor.getType() == Sensor.TYPE_GRAVITY) {
-            String strGraData = new String("");
-            strGraData += "GRAVITY\n";
-            strGraData += "x : " + event.values[0] + "\n";
-            strGraData += "y : " + event.values[1] + "\n";
-            strGraData += "z : " + event.values[2] + "\n";
-            vwGraData.setText(strGraData);
-
-            if(recording) {
-                strGraFile += event.timestamp + ";" +
+        if (sensor.getType() == Sensor.TYPE_GRAVITY ||
+                sensor.getType() == Sensor.TYPE_ACCELEROMETER ||
+                sensor.getType() == Sensor.TYPE_ROTATION_VECTOR)
+        {
+            String strSensorData = new String("");
+            String strSensorFile = new String("");
+            strSensorData += sensor.getName() + "\n";
+            strSensorData += "x : " + event.values[0] + "\n";
+            strSensorData += "y : " + event.values[1] + "\n";
+            strSensorData += "z : " + event.values[2] + "\n";
+            strSensorFile += (event.timestamp - referenceTime) + ";" +
                     event.values[0] + ";" +
                     event.values[1] + ";" +
                     event.values[2] + "\n";
-            }
-        }
-        if(sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION) {
-            String strAccData = new String("");
-            strAccData += "LINEAR ACCELERATION\n";
-            strAccData += "x : " + event.values[0] + "\n";
-            strAccData += "y : " + event.values[1] + "\n";
-            strAccData += "z : " + event.values[2] + "\n";
-            vwAccData.setText(strAccData);
 
-            if(recording) {
-                strAccFile += event.timestamp + ";" +
-                    event.values[0] + ";" +
-                    event.values[1] + ";" +
-                    event.values[2] + "\n";
+            switch(sensor.getType()) {
+                case Sensor.TYPE_ACCELEROMETER:
+                    vwAccData.setText(strSensorData);
+                    vwAccData.append(referenceTime + "\n");
+                    vwAccData.append(event.timestamp + "\n");
+                    vwAccData.append((event.timestamp-referenceTime) + "\n");
+                    if(recording)
+                        strAccFile += strSensorFile;
+                    break;
+                case Sensor.TYPE_GRAVITY:
+                    vwGraData.setText(strSensorData);
+                    if(recording)
+                        strGraFile += strSensorFile;
+                    break;
+                case Sensor.TYPE_ROTATION_VECTOR:
+                    vwOriData.setText(strSensorData);
+                    if(recording)
+                        strOriFile += strSensorFile;
+                    break;
             }
         }
-        if (sensor.getType() == Sensor.TYPE_ROTATION_VECTOR) {
-            String strOriData = new String("");
-            strOriData += "ORIENTATION\n";
-            strOriData += "x : " + event.values[0] + "\n";
-            strOriData += "y : " + event.values[1] + "\n";
-            strOriData += "z : " + event.values[2] + "\n";
-            vwOriData.setText(strOriData);
-
-            if(recording) {
-                strOriFile += event.timestamp + ";" +
-                    event.values[0] + ";" +
-                    event.values[1] + ";" +
-                    event.values[2] + "\n";
-            }
-        }
-        /*
-        else {
-            strAccData += "did not get sensor types!";
-        }
-        */
 
     }
 
